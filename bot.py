66b77ee5c789fece3ceda1d694359406cc30360d
pragyan06 <pragyan0888@gmail.com>
2026-04-11 03:28:14 +0530

    Added database setup

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import config
-
+from data import database
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -9,6 +9,7 @@ bot = commands.Bot(command_prefix=config.PREFIX, intents=intents)
 
 @bot.event
 async def on_ready():
+    await database.setup()
     await bot.load_extension("cogs.heist")
     await bot.load_extension("cogs.gambling")
     await bot.load_extension("cogs.economy")
